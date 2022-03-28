5dc49b13547a1cf7277858e8d73727a8ffc7980b
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2022-03-28 13:51:55 -0400

    adfadsf

---
+++ msksi/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+<header id="navbar">
+<h1 class="logo">MSKSI</h1>
+<div class="nav"> 
+    <router-link to="/"> Home </router-link>
+    <router-link to="/Aboutus"> About Us   </router-link>
+    <router-link to="/Calendar"> Calendar </router-link>
+    <router-link to="/Gallery"> Gallery </router-link>
+    <router-link to="/Resources"> Resources </router-link>
+    <router-link to="/Joinus"> Join Us </router-link>
+</div>
+</header>
+</template>
+
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
+
+<style scoped>
+
+</style>
--- msksi/layouts/default.vue
+++ msksi/layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <Menu />
-    <DesktopMenu />
+<Navbar />
 
     <Nuxt />
   </div>
