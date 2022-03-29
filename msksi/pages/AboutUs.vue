fc526852f81cb9dd0d7b27a291d6d8f6c3a8111d
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2022-03-28 23:06:50 -0400

    :)

--- msksi/pages/AboutUs.vue
+++ msksi/pages/AboutUs.vue
@@ -1,13 +1,12 @@
 <template>
 <div class="aboutus">
 <Navbar />
-<h2> About Us</h2>
 </div>
 
 </template>
 
 <script>
 export default {
-  name: 'AboutUs',
+  name: 'NuxtAboutUs',
 }
 </script>
--- msksi/pages/index.vue
+++ msksi/pages/index.vue
@@ -1,12 +1,9 @@
 <template>
 <Navbar />
-<Bottom />
 </template>
 
 <script>
-import Bottom from '../components/Bottom.vue'
 export default {
-  components: { Bottom },
   name: 'IndexPage',
 }
 </script>
