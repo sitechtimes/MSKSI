36a829a78e2722b21a44a5e51ac5f50c7c3970ed
vvwenv <90654380+vvwenv@users.noreply.github.com>
2022-04-05 14:00:19 -0400

    nav menu not finished :C

--- msksi/components/Menu.vue
+++ msksi/components/Menu.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="menu">
-    <button class="menuBtn" @click="toggleMenu()" ref="menuBtn">
-      <div class="line one"></div>
-      <div class="line two"></div>
-      <div class="line three"></div>
-    </button>
-    <div id="dropdown" class="dropdown-content">
-      <router-link class="menuRouter" to="/Home"> Home </router-link>
-      <router-link class="menuRouter" to="/AboutUs"> About Us </router-link>
-      <router-link class="menuRouter" to="/Calendar"> Calendar </router-link>
-      <router-link class="menuRouter" to="/Gallery"> Gallery </router-link>
-      <router-link class="menuRouter" to="/Resources"> Resources </router-link>
-      <router-link class="menuRouter" to="/JoinUs"> Join Us </router-link>
+    <div class="thing">
+      <div id="dropdown" class="dropdown-content">
+        <router-link class="menuRouter" to="/Home"> Home </router-link>
+        <router-link class="menuRouter" to="/AboutUs"> About Us </router-link>
+        <router-link class="menuRouter" to="/Calendar"> Calendar </router-link>
+        <router-link class="menuRouter" to="/Gallery"> Gallery </router-link>
+        <router-link class="menuRouter" to="/Resources">
+          Resources
+        </router-link>
+        <router-link class="menuRouter" to="/JoinUs"> Join Us </router-link>
+        <div class="thingss">
+          <div class="twoo">
+            <button class="menuBtn" @click="toggleMenu()" ref="menuBtn">
+              <div class="line one"></div>
+              <div class="line two"></div>
+              <div class="line three"></div>
+            </button>
+          </div>
+          <div class="threee"></div>
+          <div class="fourr"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
