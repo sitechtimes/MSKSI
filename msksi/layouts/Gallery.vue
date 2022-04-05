f2d4aa777025517c5fb58e7b904a428ebddb7054
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2022-04-05 13:49:05 -0400

    aaa

--- msksi/layouts/Gallery.vue
+++ msksi/layouts/Gallery.vue
@@ -3,14 +3,19 @@
 <Navbar />
 <div class="Gallerytop">
     <h2 class="heading"> Photo Gallery</h2>
+    <h3 class="subheading">Album Links</h3>
 </div>
 <Footer />
   </div>
 </template>
-<style>
+<style scoped>
 .Gallerytop{
     height: 65rem;
     background-color: var(--navyblue);
+}
+.subheading{
+    color: white;
+    text-align: center;
 }
 
 </style>
