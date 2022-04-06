b48f4681c43317eb6984569bdef336e8cc7fc746
vvwenv <90654380+vvwenv@users.noreply.github.com>
2022-04-06 13:58:25 -0400

    nav menu not done

--- msksi/components/Menu.vue
+++ msksi/components/Menu.vue
@@ -2,23 +2,44 @@
   <div class="menu">
     <div class="thing">
       <div id="dropdown" class="dropdown-content">
-        <router-link class="menuRouter" to="/Home"> Home </router-link>
-        <router-link class="menuRouter" to="/AboutUs"> About Us </router-link>
-        <router-link class="menuRouter" to="/Calendar"> Calendar </router-link>
-        <router-link class="menuRouter" to="/Gallery"> Gallery </router-link>
-        <router-link class="menuRouter" to="/Resources">
-          Resources
-        </router-link>
-        <router-link class="menuRouter" to="/JoinUs"> Join Us </router-link>
         <div class="thingss">
           <div class="twoo">
             <button class="menuBtn" @click="toggleMenu()" ref="menuBtn">
-              <div class="line one"></div>
-              <div class="line two"></div>
-              <div class="line three"></div>
+              <div class="line"></div>
+              <div class="line"></div>
+              <div class="line"></div>
             </button>
           </div>
-          <div class="threee"></div>
+          <div class="threee">
+            <li>
+              <router-link class="menuRouter" to="/Home"> Home </router-link>
+            </li>
+            <li>
+              <router-link class="menuRouter" to="/AboutUs">
+                About Us
+              </router-link>
+            </li>
+            <li>
+              <router-link class="menuRouter" to="/Calendar">
+                Calendar
+              </router-link>
+            </li>
+            <li>
+              <router-link class="menuRouter" to="/Gallery">
+                Gallery
+              </router-link>
+            </li>
+            <li>
+              <router-link class="menuRouter" to="/Resources">
+                Resources
+              </router-link>
+            </li>
+            <li>
+              <router-link class="menuRouter" to="/JoinUs">
+                Join Us
+              </router-link>
+            </li>
+          </div>
           <div class="fourr"></div>
         </div>
       </div>
