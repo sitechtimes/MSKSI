a32a3d579147e481ac8d4789aa5d072c452086c6
uh-karsi <90662606+uh-karsi@users.noreply.github.com>
2022-04-13 13:12:34 -0400

    hm

--- msksi/layouts/AboutUs.vue
+++ msksi/layouts/AboutUs.vue
@@ -1,89 +1,99 @@
 <template>
-      <div>
-<Navbar />
-<div class="AboutUsTop">
-<h2 class="heading"> <span class="our">Our</span> <span class="mission">Mission</span></h2>
-<h3 class="text">My Sister’s Keeper Staten Island (MSKSI) founded in November 2018, creates a support system to build a community for social, emotional, and academic support and leadership empowerment for MSKSI members from elementary to high school students in District 31. MSKSI fosters opportunities to build leadership, encourage sisterhood and build inner confidence (self-esteem) through workshops, sister circles, leadership events and community-based projects. My Sister’s Keeper Staten Island empowers young people to become impactful/inspiring, self-motivating, powerful transformative leaders. Giving a safe space and opportunity to our marginalized MSKSI young people in order to have a voice, respect and rapport in a diverse community</h3>
-</div>
-<div class="AboutUsMid">
-  <div class="left">
-    <h2 class=" v msksi"> MSKSI</h2>
-    <h2 class="v vision"> VISION</h2>
+  <div>
+    <Navbar />
+    <div class="AboutUsTop">
+      <h2 class="heading">
+        <span class="our">Our</span> <span class="mission">Mission</span>
+      </h2>
+      <h4 class="text">
+        My Sister’s Keeper Staten Island (MSKSI) founded in November 2018,
+        creates a support system to build a community for social, emotional, and
+        academic support and leadership empowerment for MSKSI members from
+        elementary to high school students in District 31. MSKSI fosters
+        opportunities to build leadership, encourage sisterhood and build inner
+        confidence (self-esteem) through workshops, sister circles, leadership
+        events and community-based projects. My Sister’s Keeper Staten Island
+        empowers young people to become impactful/inspiring, self-motivating,
+        powerful transformative leaders. Giving a safe space and opportunity to
+        our marginalized MSKSI young people in order to have a voice, respect
+        and rapport in a diverse community.
+      </h4>
+    </div>
+    <div class="AboutUsMid">
+      <div class="left">
+        <h2 class="v msksi">MSKSI</h2>
+        <h2 class="v vision">VISION</h2>
+      </div>
+      <div id="right"></div>
+    </div>
 
-  </div>
-  <div id="right">
-
-  </div>
-</div>
-
-<Footer />
+    <Footer />
   </div>
 </template>
 <style scoped>
-#right{
+#right {
   width: 50%;
-margin-left: 50%;
+  margin-left: 50%;
   background-color: blue;
-    font-family: 'Black Han Sans'; 
-    color: var(--navyblue);
-    font-size: var(--h4);
+  font-family: 'Black Han Sans';
+  color: var(--navyblue);
+  font-size: var(--h4);
 }
-.goal{
+.goal {
   color: var(--navyblue);
 }
-.v{
+.v {
   margin-top: auto;
   margin-bottom: auto;
   display: block;
 }
-.vision{
+.vision {
   color: var(--navyblue);
   font-size: var(--h1);
-    margin-top: 30rem;
-
+  margin-top: 30rem;
 }
-.msksi{
+.msksi {
   color: var(--darkpink);
   font-size: var(--h1);
   margin-top: 50rem;
 }
-.AboutUsMid{
+.AboutUsMid {
   text-align: center;
   background-color: white;
   height: 100rem;
 }
-.left{
+.left {
   width: 50%;
   height: 100rem;
-    font-family: 'Black Han Sans';
+  font-family: 'Black Han Sans';
 }
-.our{
-color: var(--darkpink);
-font-size: var(--h2);
+.our {
+  color: var(--darkpink);
+  font-size: var(--h2);
 }
-.mission{
-color: var(--yellow);
-font-size: var(--h2);
+.mission {
+  color: var(--yellow);
+  font-size: var(--h2);
 }
-.AboutUsTop{
-    background-color: var(--navyblue);
-    height: 100rem;
+.AboutUsTop {
+  background-color: var(--navyblue);
+  height: 100rem;
 }
-.text{
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 10rem;
-    margin-bottom: auto;
-    font-family: 'Cairo', sans-serif;
-    color: var(--white);
-    text-align: left;
-    font-size: var(--h3);
-    width: 90%;
-    justify-content: center;
+.text {
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 10rem;
+  margin-bottom: auto;
+  font-family: 'Cairo', sans-serif;
+  color: var(--white);
+  text-align: left;
+  font-size: var(--h4);
+  width: 90%;
+  justify-content: center;
 }
 @media only screen and (max-width: 1500px) {
-.text{
-  margin-top: 3rem;
-}
+  .text {
+    margin-top: 3rem;
+  }
 }
 </style>
