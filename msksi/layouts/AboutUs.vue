81a474c57529eb8589e3689b928f18a30edc5ada
vvwenv <90654380+vvwenv@users.noreply.github.com>
2022-04-20 19:19:12 -0400

    about us video

--- msksi/layouts/AboutUs.vue
+++ msksi/layouts/AboutUs.vue
@@ -1,143 +1,161 @@
 <template>
-      <div>
-<Navbar />
-<div class="AboutUsTop">
-<h2 class="heading"> <span class="h our">Our</span> <span class="h mission">Mission</span></h2>
-<h3 class="text">My Sister’s Keeper Staten Island (MSKSI) founded in November 2018, creates a support system to build a community for social, emotional, and academic support and leadership empowerment for MSKSI members from elementary to high school students in District 31. MSKSI fosters opportunities to build leadership, encourage sisterhood and build inner confidence (self-esteem) through workshops, sister circles, leadership events and community-based projects. My Sister’s Keeper Staten Island empowers young people to become impactful/inspiring, self-motivating, powerful transformative leaders. Giving a safe space and opportunity to our marginalized MSKSI young people in order to have a voice, respect and rapport in a diverse community</h3>
-</div>
-<div class="AboutUsMid">
-  <div class="left">
-    <h2 class=" v msksi"> MSKSI</h2>
-    <h2 class="v vision"> VISION</h2>
+  <div>
+    <Navbar />
+    <div class="AboutUsTop">
+      <h2 class="heading">
+        <span class="h our">Our</span> <span class="h mission">Mission</span>
+      </h2>
+      <h3 class="text">
+        My Sister’s Keeper Staten Island (MSKSI) founded in November 2018,
+        creates a support system to build a community for social, emotional, and
+        academic support and leadership empowerment for MSKSI members from
+        elementary to high school students in District 31. MSKSI fosters
+        opportunities to build leadership, encourage sisterhood and build inner
+        confidence (self-esteem) through workshops, sister circles, leadership
+        events and community-based projects. My Sister’s Keeper Staten Island
+        empowers young people to become impactful/inspiring, self-motivating,
+        powerful transformative leaders. Giving a safe space and opportunity to
+        our marginalized MSKSI young people in order to have a voice, respect
+        and rapport in a diverse community
+      </h3>
+    </div>
+    <div class="AboutUsMid">
+      <div class="left">
+        <h2 class="v msksi">MSKSI</h2>
+        <iframe
+          id="video"
+          src="https://drive.google.com/file/d/1JlT8gnYTEr3YedSs970C7dVHHq5K43eM/preview"
+          width="600"
+          height="400"
+          allow="autoplay"
+          poster="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png"
+        ></iframe>
+        <h2 class="v vision">VISION</h2>
+      </div>
+      <div id="right"></div>
+    </div>
 
-  </div>
-  <div id="right">
-
-  </div>
-</div>
-
-<Footer />
+    <Footer />
   </div>
 </template>
 <style scoped>
-#right{
+#right {
   width: 50%;
-margin-left: 50%;
+  margin-left: 50%;
   background-color: blue;
-    font-family: 'Black Han Sans'; 
-    color: var(--navyblue);
-    font-size: var(--h4);
+  font-family: 'Black Han Sans';
+  color: var(--navyblue);
+  font-size: var(--h4);
 }
-.goal{
+.goal {
   color: var(--navyblue);
 }
-.v{
+.v {
   margin-top: auto;
   margin-bottom: auto;
   display: block;
 }
-.vision{
+.vision {
   color: var(--navyblue);
   font-size: var(--h1);
-    margin-top: 30rem;
 }
-.msksi{
+.msksi {
   color: var(--darkpink);
   font-size: var(--h1);
   margin-top: 50rem;
 }
-.AboutUsMid{
+.AboutUsMid {
   text-align: center;
   background-color: white;
   height: 100rem;
 }
-.left{
+.left {
   width: 50%;
   height: 100rem;
-    font-family: 'Black Han Sans';
+  font-family: 'Black Han Sans';
 }
-.our{
-color: var(--darkpink);
-font-size: var(--h2);
+.our {
+  color: var(--darkpink);
+  font-size: var(--h2);
 }
-.mission{
-color: var(--yellow);
-font-size: var(--h2);
+.mission {
+  color: var(--yellow);
+  font-size: var(--h2);
 }
-.AboutUsTop{
-    background-color: var(--navyblue);
-    height: 100rem;
+.AboutUsTop {
+  background-color: var(--navyblue);
+  height: 100rem;
 }
-.text{
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 10rem;
-    margin-bottom: auto;
-    font-family: 'Cairo', sans-serif;
-    color: var(--white);
-    text-align: left;
-    font-size: var(--h3);
-    width: 90%;
-    justify-content: center;
+.text {
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 10rem;
+  margin-bottom: auto;
+  font-family: 'Cairo', sans-serif;
+  color: var(--white);
+  text-align: left;
+  font-size: var(--h3);
+  width: 90%;
+  justify-content: center;
+}
+#video {
+  padding: 5rem;
+  border-style: none;
 }
 @media only screen and (max-width: 810px) {
-.text{
-  margin-top: 2rem;
-  font-size: var(--h5);
-  width: 80%;
+  .text {
+    margin-top: 2rem;
+    font-size: var(--h5);
+    width: 80%;
+  }
+  .AboutUsTop {
+    height: 125rem;
+  }
+  .vision {
+    color: var(--navyblue);
+    font-size: var(--h3);
+  }
+  .msksi {
+    color: var(--darkpink);
+    font-size: var(--h3);
+    margin-top: 50rem;
+  }
+  .heading {
+    margin-left: 3rem;
+    padding-top: 10.5rem;
+  }
+  .h {
+    font-size: var(--h3);
+    margin-top: 0;
+    padding-top: 0;
+  }
 }
-.AboutUsTop{
-      height: 125rem;
-}
-.vision{
-  color: var(--navyblue);
-  font-size: var(--h3);
-    margin-top: 30rem;
-
-}
-.msksi{
-  color: var(--darkpink);
-  font-size: var(--h3);
-  margin-top: 50rem;
-}
-.heading{
-margin-left: 3rem;
-padding-top: 10.5rem;
-}
-.h{
-  font-size: var(--h3);
-margin-top: 0;
-padding-top: 0;
-}
-}
-@media only screen and (max-width: 1500px){
-  .AboutUsTop{
+@media only screen and (max-width: 1500px) {
+  .AboutUsTop {
     height: 85rem;
-}
-.text{
-  margin-top: 5rem;
-  font-size: var(--h4);
-}
-.AboutUsMid{
-  text-align: center;
-  background-color: white;
-  height: 56rem;
-}
-.left{
-  width: 50%;
-  height: 65rem;
+  }
+  .text {
+    margin-top: 5rem;
+    font-size: var(--h4);
+  }
+  .AboutUsMid {
+    text-align: center;
+    background-color: white;
+    height: 56rem;
+  }
+  .left {
+    width: 50%;
+    height: 65rem;
     font-family: 'Black Han Sans';
+  }
+  .msksi {
+    color: var(--darkpink);
+    font-size: var(--h1);
+    margin-top: 17rem;
+  }
+  .vision {
+    color: var(--navyblue);
+    font-size: var(--h1);
+  }
 }
-.msksi{
-  color: var(--darkpink);
-  font-size: var(--h1);
-  margin-top: 17rem;
-}
-.vision{
-  color: var(--navyblue);
-  font-size: var(--h1);
-    margin-top: 20rem;
-}
-}
-
 </style>
