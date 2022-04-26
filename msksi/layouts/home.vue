c71fde060019db540b6229b7455b24d5e92b2bb7
uh-karsi <90662606+uh-karsi@users.noreply.github.com>
2022-04-26 13:11:08 -0400

    purrrr

--- msksi/layouts/home.vue
+++ msksi/layouts/home.vue
@@ -1,46 +1,65 @@
 <template>
   <div>
-<Navbar />
-<div class="hometop">
-    <h2 class="heading mysis"> My Sister’s</h2>
-    <h2 class="heading keeper"> Keeper</h2>
-    <h3 class="si">Staten Island</h3>
-</div>
+    <Navbar />
+    <div class="hometop">
+      <div class="alignright">
+        <h2 class="heading mysis">My Sister’s</h2>
+        <h2 class="heading keeper">Keeper</h2>
+      </div>
 
-<Footer />
+      <h3 class="si">Staten Island</h3>
+    </div>
+
+    <div class="homespotlight"></div>
+    <Footer />
   </div>
 </template>
 <style scoped>
-.mysis{
+.alignright {
+  border: 2px solid red;
+  width: fit-content;
+  display: flex;
+  align-content: right;
+  height: fit-content;
+  flex-direction: column;
+  transform: translateX(50%);
+}
+.mysis {
   font-size: 12rem;
-  text-align: center;
+  text-align: right;
+  padding-top: 30rem;
+}
+.keeper {
+  color: var(--darkpink);
+  font-size: 12rem;
+  text-align: right;
+
+  padding-top: 5rem;
+}
+.hometop {
+  background-color: var(--navyblue);
+  height: 100rem;
+}
+.si {
+  font-family: 'ABeeZee', sans-serif;
+  color: var(--yellow);
+  font-size: var(--h3);
   width: 90%;
-padding-top: 30rem;
+  text-align: right;
+  margin-top: 10rem;
 }
-.keeper{
+
+.homespotlight {
+  height: 10rem;
+  background-color: var(--white);
+}
+
+/* @media only screen and (max-width: 1500px) {
+  .keeper {
     color: var(--darkpink);
-     font-size: 12rem;
-     text-align: right;
-     width: 70%;
-}
-.hometop{
-    background-color: var(--navyblue);
-    height: 100rem;
-}
-.si{
-    font-family: 'ABeeZee', sans-serif;
-    color: var(--yellow);
-    font-size: var(--h3);
-    width: 90%;
+    font-size: 12rem;
     text-align: right;
-    margin-top: 10rem;
-}
-@media only screen and (max-width: 1500px) {
-    .keeper{
-    color: var(--darkpink);
-     font-size: 12rem;
-     text-align: right;
-     width: 80%;
-}
-}
+    width: 80%;
+  }
+} */
 </style>
--- msksi/pages/home.vue
+++ msksi/pages/home.vue
@@ -4,6 +4,7 @@
 
 <script>
 export default {
+  layout: 'Home',
   name: 'Home',
 }
 </script>
