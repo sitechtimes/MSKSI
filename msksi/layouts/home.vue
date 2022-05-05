604a2be18275e86b5d6c1fef41ee5cfa7ed2e5ec
uh-karsi <90662606+uh-karsi@users.noreply.github.com>
2022-05-05 13:11:44 -0400

    bro some progress B)

--- msksi/layouts/home.vue
+++ msksi/layouts/home.vue
@@ -5,24 +5,32 @@
       <div class="alignright">
         <h2 class="heading mysis">My Sister’s</h2>
         <h2 class="heading keeper">Keeper</h2>
+        <h3 class="si">Staten Island</h3>
       </div>
-
-      <h3 class="si">Staten Island</h3>
     </div>
 
-    <div class="homespotlight"></div>
+    <div class="homespotlight">
+      <img
+        id="spotlight"
+        src="https://www.yosemitelakespark.org/wp-content/uploads/2021/02/ylp-spotlight.png"
+        alt=""
+      />
+    </div>
     <Footer />
   </div>
 </template>
 <style scoped>
+.hometop {
+  background-color: var(--navyblue);
+  height: 100rem;
+}
 .alignright {
-  border: 2px solid red;
   width: fit-content;
   display: flex;
   align-content: right;
   height: fit-content;
   flex-direction: column;
-  transform: translateX(50%);
+  margin: auto;
 }
 .mysis {
   font-size: 12rem;
@@ -33,25 +41,44 @@
   color: var(--darkpink);
   font-size: 12rem;
   text-align: right;
-
   padding-top: 5rem;
 }
-.hometop {
-  background-color: var(--navyblue);
-  height: 100rem;
-}
+
 .si {
   font-family: 'ABeeZee', sans-serif;
   color: var(--yellow);
   font-size: var(--h3);
-  width: 90%;
+  width: 25rem;
   text-align: right;
   margin-top: 10rem;
+  margin-left: 75%;
 }
 
 .homespotlight {
-  height: 10rem;
+  height: 100rem;
   background-color: var(--white);
+}
+
+#spotlight {
+  margin-left: 2rem;
+  margin-top: 20rem;
+}
+
+@keyframes rotateLight {
+  0% {
+    transform: rotate(90);
+  }
+  100% {
+    transform: rotate(0);
+  }
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  #spotlight {
+    animation-name: rotateLight;
+    animation-duration: 2s;
+    animation-iteration-count: infinite;
+  }
 }
 
 /* @media only screen and (max-width: 1500px) {
