8d1ff8cfce64d30e6656033564c631577c70ab3e
vvwenv <90654380+vvwenv@users.noreply.github.com>
2022-05-18 14:00:16 -0400

    homelogo layout

--- msksi/layouts/home.vue
+++ msksi/layouts/home.vue
@@ -2,55 +2,47 @@
   <div>
     <Navbar />
     <div class="hometop">
-      <h2 class="homelogo mysis">My Sister’s</h2>
-      <h2 class="homelogo keeper">Keeper</h2>
-      <h3 class="si">Staten Island</h3>
+      <div class="mainlogo">
+        <h2 class="homelogo" id="mysis">My Sister’s</h2>
+        <h2 class="homelogo" id="keeper">Keeper</h2>
+        <h3 class="homelogo" id="si">Staten Island</h3>
+      </div>
     </div>
 
     <Footer />
   </div>
 </template>
 <style scoped>
-.homelogo {
-  margin-top: 2rem;
-}
-.mysis {
-  font-size: 13rem;
-  text-align: center;
-  margin-left: auto;
-  margin-right: auto;
-  padding-top: 30rem;
-  display: flex;
-  justify-content: center;
-  color: var(--yellow);
-  font-family: 'Black Han Sans';
-}
-.keeper {
-  color: var(--darkpink);
-  font-size: var(--h1);
-  font-family: 'Black Han Sans';
-  text-align: right;
-  width: 42.5%;
-  font-size: 13rem;
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
-  justify-content: right;
-}
-.heading {
-  margin-top: 3rem;
-}
 .hometop {
   background-color: var(--navyblue);
   height: 100rem;
 }
-.si {
+.mainlogo {
+  text-align: right;
+  font-family: 'Black Han Sans';
+}
+.homelogo {
+  margin-top: 2rem;
+  font-size: 13rem;
+  display: inline-block;
+}
+#mysis {
+  padding-top: 30rem;
+  color: var(--yellow);
+  text-align: center;
+}
+#keeper {
+  color: var(--darkpink);
+  display: block;
+}
+#si {
   font-family: 'ABeeZee', sans-serif;
   color: var(--yellow);
   font-size: var(--h2);
   width: 82%;
   text-align: right;
   margin-top: 4rem;
+  display: block;
 }
 @media only screen and (max-width: 1500px) {
   .hometop {
