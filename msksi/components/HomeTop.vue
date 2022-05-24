2d9e879154b6fff5a988ce740aa4d17afec1e158
uh-karsi <90662606+uh-karsi@users.noreply.github.com>
2022-05-24 01:29:53 -0400

    :(

--- msksi/components/HomeTop.vue
+++ msksi/components/HomeTop.vue
@@ -1,9 +1,21 @@
 <template>
-     <div class="hometop">
-      <h2 class="homelogo mysis">My Sister’s</h2>
-      <h2 class="homelogo keeper">Keeper</h2>
-      <h3 class="si">Staten Island</h3>
+  <div>
+    <div class="hometop">
+      <div class="alignright">
+        <h2 class="heading mysis">My Sister’s</h2>
+        <div class="line2">
+          <img
+            id="temp-logo"
+            src="https://media.discordapp.net/attachments/747595261395861545/971942403323871262/unknown.png"
+            alt=""
+          />
+          <h2 class="heading keeper">Keeper</h2>
+        </div>
+
+        <h3 class="si">Staten Island</h3>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -13,64 +25,68 @@ export default {
 </script>
 
 <style scoped>
-.homelogo {
-  margin-top: 2rem;
-}
-.mysis {
-  font-size: 13rem;
-  text-align: center;
-  margin-left: auto;
-  margin-right: auto;
-  padding-top: 30rem;
-  display: flex;
-  justify-content: center;
-  color: var(--yellow);
-  font-family: 'Black Han Sans';
-}
-.keeper {
-  color: var(--darkpink);
-  font-size: var(--h1);
-  font-family: 'Black Han Sans';
-  text-align: right;
-  width: 42.5%;
-  font-size: 13rem;
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
-  justify-content: right;
-}
-.heading {
-  margin-top: 3rem;
-}
 .hometop {
   background-color: var(--navyblue);
   height: 100rem;
 }
+.alignright {
+  width: fit-content;
+  display: flex;
+  align-content: right;
+  height: fit-content;
+  flex-direction: column;
+  margin: auto;
+}
+.mysis {
+  font-size: 13rem;
+  text-align: right;
+  padding-top: 30rem;
+}
+
+.line2 {
+  display: flex;
+  height: fit-content;
+  flex-direction: row;
+  justify-content: right;
+
+  align-content: center;
+  position: relative;
+}
+
+.keeper {
+  color: var(--darkpink);
+  font-size: 13rem;
+  text-align: right;
+  padding-top: 3rem;
+  width: 50rem;
+  margin-right: 0%;
+}
+
+#temp-logo {
+  /* border: solid 2px red; */
+  width: auto;
+  height: 18rem;
+  position: absolute;
+  bottom: 5%;
+  right: 57rem;
+}
+
 .si {
   font-family: 'ABeeZee', sans-serif;
   color: var(--yellow);
-  font-size: var(--h2);
-  width: 82%;
+  font-size: var(--h3);
+  width: 35rem;
   text-align: right;
-  margin-top: 4rem;
+  margin-top: 10rem;
+  margin-left: 75%;
 }
-@media only screen and (max-width: 1500px) {
-  .hometop {
-    height: 80rem;
+
+/* @media only screen and (max-width: 1500px) {
+  .keeper {
+    color: var(--darkpink);
+    font-size: 12rem;
+    text-align: right;
+    width: 80%;
   }
-}
-@media only screen and (max-width: 1032px) {
-  .hometop {
-    height: auto;
-  }
-  .hometop {
-    background-color: var(--navyblue);
-    height: 100rem;
-  }
-}
-@media only screen and (max-width: 400px) {
-  .hometop {
-    height: 80rem;
-  }
-}
+} */ ;
 </style>
--- msksi/pages/home.vue
+++ msksi/pages/home.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <Navbar />
+    <HomeTop />
     <CommSpot />
     <Footer />
   </div>
 </template>
 
 <script>
+import HomeTop from '~/components/HomeTop.vue'
+
 export default {
+  components: { HomeTop },
   name: 'home',
 }
 </script>
