5f688eea3b7b621fb25ce14675025834ef351f51
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2022-05-25 13:53:54 -0400

    small screen size fixes

--- msksi/components/Footer.vue
+++ msksi/components/Footer.vue
@@ -11,15 +11,44 @@
   </div>
 </template>
 <style scoped>
+@media only screen and (max-width: 1032px) {
+  .contactus {
+    font-size: var(--h4);
+  }
+  .contacts {
+    color: var(--white);
+    margin-top: 2rem;
+    font-weight: lighter;
+    font-size: var(--h5);
+    font-family: 'ABeeZee', sans-serif;
+  }
+  #footer {
+    width: 100%;
+    padding-bottom: 2rem;
+  }
+  .buttons {
+    margin-top: 3rem;
+  }
+  .icon {
+    height: 3rem;
+    width: 3rem;
+    border-radius: 1rem;
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+}
 @media only screen and (max-width: 810px) {
   #footer {
-    height: 20rem;
+    height: 22rem;
     margin-bottom: 0;
   }
 }
 .icon{
       cursor: pointer;
+      width: 2rem;
+      height: 2rem;
 }
+
 </style>
 
 <script>
