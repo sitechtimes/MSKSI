c2c9e3a5ae0bbeaa32447c4d907d46c38e00dd14
uh-karsi <90662606+uh-karsi@users.noreply.github.com>
2022-05-27 12:56:00 -0400

    Update home.vue

--- msksi/pages/home.vue
+++ msksi/pages/home.vue
@@ -1,14 +1,11 @@
 <template>
   <div>
-    <Navbar />
     <HomeTop />
     <CommSpot />
-    <Footer />
   </div>
 </template>
 
 <script>
-
 import CommSpot from '~/components/CommSpot.vue'
 import HomeTop from '~/components/HomeTop.vue'
 export default {
