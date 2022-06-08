adbab87fa2e35dc90156323051db750519a988b8
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2022-06-08 13:59:48 -0400

    resouces

--- msksi/components/ResourcesTop.vue
+++ msksi/components/ResourcesTop.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="resourcestop">
     <h2 class="heading">Resources</h2>
+    <div class="resource">
+      <img src="https://lh5.googleusercontent.com/COsJj6QMRp1jsOd4yBIRe_xlfHZUyxfWRdsJOPpr84qrU0J6yRuou1e5PCTwNtCMER27EcClC27QdOSn68jjZ_I=w1280" alt="folder">
+    <a href=""> MSKSI Video Folder</a>
+    </div>
+  
   </div>
 </template>
 
@@ -10,12 +15,24 @@ export default {
 }
 </script>
 <style scoped>
+.resource{
+  width: 30rem;
+}
 .resourcestop {
   background-color: #333043;
   height: 100rem;
 }
 .heading {
   color: var(--yellow);
+}
+img{
+  width: 25rem;
+margin-left: 20rem;
+}
+a{
+  color: white;
+  font-size: var(--h4);
+font-family: 'Cairo', sans-serif;
 }
 @media only screen and (max-width: 810px) {
   .heading {
