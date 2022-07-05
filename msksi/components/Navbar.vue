f853be1adb1214fdc69e41ba4a564a8cb7d991f5
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2022-07-05 11:09:50 -0400

    favicon logo

--- msksi/components/Navbar.vue
+++ msksi/components/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
   <header id="navbar">
-    <h1 class="logo">MSKSI</h1>
+    <h1 class="logo"><a class="logo" href="/">MSKSI</a></h1>
     <div class="nav">
-      <router-link class="navRouter" to="/"> Home </router-link>
-      <router-link class="navRouter" to="/AboutUs"> About Us </router-link>
-      <router-link class="navRouter" to="/Calendar"> Calendar </router-link>
-      <router-link class="navRouter" to="/Gallery"> Gallery </router-link>
-      <router-link class="navRouter" to="/Resources"> Resources </router-link>
-      <router-link class="navRouter" to="/JoinUs"> Join Us </router-link>
+      <router-link class="navRouter router" to="/"> Home </router-link>
+      <router-link class="navRouter router" to="/AboutUs"> About Us </router-link>
+      <router-link class="navRouter router" to="/Calendar"> Calendar </router-link>
+      <router-link class="navRouter router" to="/Gallery"> Gallery </router-link>
+      <router-link class="navRouter router" to="/Resources"> Resources </router-link>
+      <router-link class="navRouter router" to="/JoinUs"> Join Us </router-link>
     </div>
     <Menu />
   </header>
