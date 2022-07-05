e42c1f1bd6ab803ace0b013be1c8631e722da082
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2022-07-05 10:48:20 -0400

    resources design 1500 px

--- msksi/components/ResourcesTop.vue
+++ msksi/components/ResourcesTop.vue
@@ -4,31 +4,36 @@
     <div id="resources">
     <div class="resource">
       <img src="https://i.pinimg.com/originals/67/75/9e/67759e3a6a1dd544deffe5673d021174.png">
-    <a id="link" href="https://sites.google.com/strongschools.nyc/msksi/resources#h.vefx57yqjvc2">MSKSI Video Folder</a>
+    <a target="_blank" id="link" href="https://sites.google.com/strongschools.nyc/msksi/resources#h.vefx57yqjvc2">MSKSI Video Folder</a>
     </div>
       <div class="resource">
       <img src="https://lh5.googleusercontent.com/PuV14rc2AkU5Kq_K5brxP4UFlGe3IRxLydIY-RuY7BW2j88m5dlpBAvNehiTluiAZ97wQ01GAo1VIG8IHcGqnGxu5QXMw-f3fPMGxrihsHuKhXheDXjZRkGyvrX8idFSnw=w1280">
-    <a id="link" href="https://sites.google.com/strongschools.nyc/msksi/resources#h.3zdell8jwjmb">June 11, 2021 Jamboard Vision-board Reflections</a>
+    <a target="_blank" id="link" href="https://sites.google.com/strongschools.nyc/msksi/resources#h.3zdell8jwjmb">June 11, 2021 Jamboard Vision-board Reflections</a>
     </div>
       <div class="resource">
       <img src="https://padlet-uploads.storage.googleapis.com/5587727/AEV1LKtFkyKgLU1DzT_Ocg/ef1210d4305560b0eb3acf6500d5099c.png">
-    <a id="link"  href="https://sites.google.com/strongschools.nyc/msksi/resources#h.2ejb8tqf6z7a">April 9 MSKSI Padlet for Breakouts</a>
+    <a target="_blank" id="link"  href="https://sites.google.com/strongschools.nyc/msksi/resources#h.2ejb8tqf6z7a">April 9 MSKSI Padlet for Breakouts</a>
     </div>
     <div class="resource">
       <img src="https://lh3.googleusercontent.com/rQQvR9fnoQP1dNAApdTanQqgs5dM5RNCrwZiiLQ38xvIxqO0GFo5ZfyQLwy2qnfnAhGEdzVgNvKXEcNZM0nCd0LTXLKZsaHn5n9IAPibIktsdhvd_QtyUHBVC-h3pdas-A=w1280">
-    <a id="link"  href="https://docs.google.com/presentation/u/0/d/17DbBKlW0LjksZ3NsXBlewAGMp1v4YNWDP85bbhJEwTQ/copy">MSKSI Journal</a>
+    <a target="_blank" id="link"  href="https://docs.google.com/presentation/u/0/d/17DbBKlW0LjksZ3NsXBlewAGMp1v4YNWDP85bbhJEwTQ/copy">MSKSI Journal</a>
     </div>
     <div class="resource">
       <img src="https://padlet-uploads.storage.googleapis.com/5587727/AEV1LKtFkyKgLU1DzT_Ocg/ef1210d4305560b0eb3acf6500d5099c.png">
-    <a id="link" href="https://docs.google.com/document/d/11fbuSuNiU4O6g6RKTeAxfixFOV89Dd1W1wO5CEcbwmQ/edit">Background-Vision Board</a>
+    <a target="_blank" id="link" href="https://docs.google.com/document/d/11fbuSuNiU4O6g6RKTeAxfixFOV89Dd1W1wO5CEcbwmQ/edit">Background-Vision Board</a>
     </div>
     <div class="resource">
-      <img src="">
-    <a id="link" href="">VoiceThread from the MSKSI Meeting January 8, 2021</a>
+    <a target="_blank" id="link" href="">VoiceThread from the MSKSI Meeting January 8, 2021</a>
+      <ul>
+      <h4 class="item">Gathering inspiration</h4>
+      <h4 class="item">Thinking outside the box and failing </h4>
+      <h4 class="item">Ideas for journaling and goal setting</h4>
+      <h4 class="item">Projects & connecting with others</h4>   
+        </ul>
     </div>
     <div class="resource">
       <img src="https://lh4.googleusercontent.com/nE2JYKfW5D-icd_xuFx0rJ2nFvSAC6qHX7JF6dN-CTDSQC9Sp0H7jz-2OMVE8DGixIW8V91uNu82W36frdhbBAWFvBeRNOVU3o_k2uBCyIF4UzEflJMFqAnTPHb_Rv7biQ=w1280">
-    <a id="link" href="https://jamboard.google.com/d/1I0G1z3-7R96nD9EAgRI4ncikXWkw8AhpdrRZC3JOtIw/viewer">Jamboard January 8, 2021 Gratitude Prompts link </a>
+    <a target="_blank" id="link" href="https://jamboard.google.com/d/1I0G1z3-7R96nD9EAgRI4ncikXWkw8AhpdrRZC3JOtIw/viewer">Jamboard January 8, 2021 Gratitude Prompts link </a>
     </div>
     <div class="resource">
     <a id="link" href="https://sites.google.com/strongschools.nyc/msksi/resources#h.xere2vvav6n9">Let's  hear from you! </a>
@@ -88,13 +93,24 @@ display: block;
 margin-top: 3rem;
 }
 #link{
-    color: #333043;
-  font-size: var(--h5);
+color: #333043;
+font-size: var(--h5);
 font-family: 'Cairo', sans-serif;
-  width: 80%;
-  margin-left: auto;
+width: 80%;
+margin-left: auto;
 margin-right: auto;
 margin-top: 2rem;
+}
+.item{
+color: #333043;
+font-family: 'Cairo', sans-serif;
+text-align: left;
+font-size: var(--h6);
+}
+ul{
+  margin-top: .2rem;
+  margin-left: auto;
+  margin-right: auto;
 }
 @media only screen and (max-width: 1500px){
   .resource{
@@ -113,7 +129,7 @@ margin-top: 2rem;
   color: var(--yellow);
 }
 img{
-  width: 20rem;
+  height: 20rem;
 margin-left: 20rem;
 }
 #link{
@@ -122,8 +138,9 @@ font-size: var(--h5);
 font-family: 'Cairo', sans-serif;
 text-align: center;
 }
+
 }
-@media only screen and (max-width: 810px) {
+@media only screen and (max-width: 420px) {
   .heading {
     font-size: var(--h3);
     margin-left: 4rem;
