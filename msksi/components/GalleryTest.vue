0e47691934741985dcc82382d3a7243f3ecac757
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2022-07-07 08:43:54 -0400

    test

---
+++ msksi/components/GalleryTest.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="Gallery">
+    <div class="Albums">
+        <Album 
+        class="polaroid" 
+        v-for="album in albums"
+        :key="album.albumname" 
+        :albumname="album.albumname"
+        :albumimg="album.img"
+        :albumlink="album.link"
+        />
+    </div>
+
+</div>
+    
+</template>
+<script>
+export default {
+    name:'GalleryTest',
+    components:{
+
+    },
+    data(){
+        return{
+            albums:[],
+        }
+    },
+    async fetch(){
+        this.albums = await this.$content(`album`).fetch()
+    }
+}
+</script>
--- msksi/pages/Gallery.vue
+++ msksi/pages/Gallery.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <GalleryTop />
+    <GalleryTest />
   </div>
 </template>
 
