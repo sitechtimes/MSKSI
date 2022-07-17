9958a4cd0322a9c7cad2829c697d6c2617bffe9a
C4SSL <cassidyl17@nycstudents.net>
2022-07-17 19:22:20 -0400

    displays pictures + buttons
    needs styling

--- msksi/components/newcarousel.vue
+++ msksi/components/newcarousel.vue
@@ -3,10 +3,10 @@
         <div class="cardList">
             <button @click="swapCards(left)" class="left btn">
                 <div class="icon">
-                    <P>L</P>
+                    <p>L</p>
                 </div>
             </button>
-                <div class="card-wrapper">
+                <div class="card-wrapper" ref="card-wrapper">
                     <div class="current-card">
                         <div class="cardimg">
                             <img src="https://randomwordgenerator.com/img/picture-generator/57e5d4444d50ab14f1dc8460962e33791c3ad6e04e507440752f73dd944bc6_640.jpg" alt="">
@@ -23,9 +23,9 @@
                         </div>
                     </div>
                 </div>
-                <button class="right btn">
+                <button @click="swapCards(right)" class="right btn">
                 <div class="icon">
-                    <P>L</P>
+                    <p>R</p>
                 </div>
             </button>
         </div>
@@ -33,11 +33,11 @@
 </template>
 
 <script>
-const cardsContainerEl = this.$refs["card-wrapper"]
 export default {
     name: 'CarouselNew',
     methods: {
         swapCards(direction){
+            const cardsContainerEl = this.$refs["card-wrapper"]
             const currentCardEl = cardsContainerEl.querySelector(".current-card");
             const prevCardEl = cardsContainerEl.querySelector(".prev-card");
             const nextCardEl = cardsContainerEl.querySelector(".next-card");
