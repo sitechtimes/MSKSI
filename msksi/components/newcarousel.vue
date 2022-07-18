c1db9f9d0d53c9d4a0aa7036420ccc7489fc38ee
C4SSL <cassidyl17@nycstudents.net>
2022-07-18 12:42:09 -0400

    l

--- msksi/components/newcarousel.vue
+++ msksi/components/newcarousel.vue
@@ -3,21 +3,21 @@
         <div class="cardList">
             <button @click="swapCards(left)" class="left-btn">
                 <div class="icon">
-                    <p>L</p>
+                    <img src="" alt="">
                 </div>
             </button>
                 <div class="card-wrapper" ref="card-wrapper">
-                    <div class="current--card">
+                    <div class="current--card" ref="current--card">
                         <div class="cardimg">
                             <img src="https://randomwordgenerator.com/img/picture-generator/57e5d4444d50ab14f1dc8460962e33791c3ad6e04e507440752f73dd944bc6_640.jpg" alt="">
                         </div>
                     </div>
-                    <div class="next--card">
+                    <div class="next--card" ref="next--card">
                         <div class="cardimg">
                             <img src="https://randomwordgenerator.com/img/picture-generator/54e3dd404c5aaf14f1dc8460962e33791c3ad6e04e5074417c2f7dd6954cc1_640.jpg" alt="">
                         </div>
                     </div>
-                    <div class="prev--card">
+                    <div class="prev--card" ref="prev--card">
                         <div class="cardimg">
                             <img src="https://randomwordgenerator.com/img/picture-generator/53e3d44a4950aa14f1dc8460962e33791c3ad6e04e507440722d72d5944ccd_640.jpg" alt="">
                         </div>
@@ -46,8 +46,6 @@ export default {
                 currentCardEl.classList.remove(".current--card")
                 prevCardEl.classList.remove(".prev--card")
                 nextCardEl.classList.remove(".next--card")
-
-                currentCardEl.style.zIndex = "50";
 
                 if (direction === "right") {
                     prevCardEl.style.zIndex = "20";
@@ -106,13 +104,12 @@ button{
     height: 100%;
     perspective: 1000px;
 }
-
 .current--card {
     width: 100%;
     height: 100%;
     display: inline-block;
     user-select: none;
-    z-index: -1;
+    z-index: 50;
     position: relative;
 }
 .prev--card {
