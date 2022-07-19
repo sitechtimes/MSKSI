7f08a5522a57a2ef28c2cb106d8206a9fcb04de9
C4SSL <cassidyl17@nycstudents.net>
2022-07-19 09:55:59 -0400

    IT WORKS

--- msksi/components/newcarousel.vue
+++ msksi/components/newcarousel.vue
@@ -1,150 +1,161 @@
 <template>
-    <div class="app">
-        <div class="cardList">
-            <button @click="swapCards(left)" class="left-btn">
-                <div class="icon">
-                    <img src="" alt="">
-                </div>
-            </button>
-                <div class="card-wrapper" ref="card-wrapper">
-                    <div class="current--card" ref="current--card">
-                        <div class="cardimg">
-                            <img src="https://randomwordgenerator.com/img/picture-generator/57e5d4444d50ab14f1dc8460962e33791c3ad6e04e507440752f73dd944bc6_640.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="next--card" ref="next--card">
-                        <div class="cardimg">
-                            <img src="https://randomwordgenerator.com/img/picture-generator/54e3dd404c5aaf14f1dc8460962e33791c3ad6e04e5074417c2f7dd6954cc1_640.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="prev--card" ref="prev--card">
-                        <div class="cardimg">
-                            <img src="https://randomwordgenerator.com/img/picture-generator/53e3d44a4950aa14f1dc8460962e33791c3ad6e04e507440722d72d5944ccd_640.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-                <button @click="swapCards(right)" class="right-btn">
-                <div class="icon">
-                    <p>R</p>
-                </div>
-            </button>
+  <div class="NewCarousel">
+    <div class="cardList">
+      <button @click="swapCards(left)" class="left-btn">
+        <div class="icon">
+          <img src="" alt="" />
         </div>
+      </button>
+      <div class="card-wrapper" ref="card-wrapper">
+        <div class="current--card" ref="current--card">
+          <div class="cardimg">
+            <img
+              src="https://randomwordgenerator.com/img/picture-generator/57e5d4444d50ab14f1dc8460962e33791c3ad6e04e507440752f73dd944bc6_640.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="next--card" ref="next--card">
+          <div class="cardimg">
+            <img
+              src="https://randomwordgenerator.com/img/picture-generator/54e3dd404c5aaf14f1dc8460962e33791c3ad6e04e5074417c2f7dd6954cc1_640.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="prev--card" ref="prev--card">
+          <div class="cardimg">
+            <img
+              src="https://randomwordgenerator.com/img/picture-generator/53e3d44a4950aa14f1dc8460962e33791c3ad6e04e507440722d72d5944ccd_640.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <button @click="swapCards(right)" class="right-btn">
+        <div class="icon">
+          <p>R</p>
+        </div>
+      </button>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'CarouselNew',
-    props: ['left', 'right'],
-    methods: {
-        swapCards(direction){
-            console.log("poo")
-            const cardsContainerEl = this.$refs["card-wrapper"]
-            const currentCardEl = cardsContainerEl.querySelector(".current--card");
-            const prevCardEl = cardsContainerEl.querySelector(".prev--card");
-            const nextCardEl = cardsContainerEl.querySelector(".next--card");
-            function swapCardsClass(){
-                currentCardEl.classList.remove(".current--card")
-                prevCardEl.classList.remove(".prev--card")
-                nextCardEl.classList.remove(".next--card")
-
-                if (direction === "right") {
-                    prevCardEl.style.zIndex = "20";
-                    nextCardEl.style.zIndex = "30";
-
-                    currentCardEl.classList.add(".prev--card");
-                    prevCardEl.classList.add(".next--card");
-                    nextCardEl.classList.add(".current--card");
-                } else if (direction === "left"){
-                    prevCardEl.style.zIndex = "30";
-                    nextCardEl.style.zIndex = "20";
-
-                    currentCardEl.classList.add(".next--card");
-                    prevCardEl.classList.add(".current--card");
-                    nextCardEl.classList.add("prev--card");
-                }
-            }
-            swapCardsClass();
-        }
+  name: 'CarouselNew',
+  data() {
+    return {
+      right: 'right',
+      left: 'left',
     }
+  },
+  methods: {
+    swapCards(direction) {
+      const cardsContainerEl = this.$refs['card-wrapper']
+      function swapCardsClass() {
+        const currentCardEl = cardsContainerEl.querySelector('.current--card')
+        const prevCardEl = cardsContainerEl.querySelector('.prev--card')
+        const nextCardEl = cardsContainerEl.querySelector('.next--card')
+        currentCardEl.classList.remove('current--card')
+        prevCardEl.classList.remove('prev--card')
+        nextCardEl.classList.remove('next--card')
+
+        if (direction === 'right') {
+          prevCardEl.style.zIndex = '20'
+          nextCardEl.style.zIndex = '30'
+
+          currentCardEl.classList.add('prev--card')
+          prevCardEl.classList.add('next--card')
+          nextCardEl.classList.add('current--card')
+        } else if (direction === 'left') {
+          prevCardEl.style.zIndex = '30'
+          nextCardEl.style.zIndex = '20'
+
+          currentCardEl.classList.add('next--card')
+          prevCardEl.classList.add('current--card')
+          nextCardEl.classList.add('prev--card')
+        }
+      }
+      swapCardsClass()
+    },
+  },
 }
 </script>
 
 <style scoped>
-.app{
-    position: relative;
-    width: 100%;
-    height: 90rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: var(--navyblue);
-    overflow: hidden;
+.NewCarousel {
+  position: relative;
+  width: 100%;
+  height: 95rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--navyblue);
+  overflow: hidden;
 }
-button{
-    --btn-size: 35px;
-    border: none;
-    background: none;
-    position: absolute;
-    background-color: red;
-    height: var(--btn-size);
-    width: var(--btn-size);
-    top: 50%;
-    transform: translate(-50%);
-    z-index: 100;
+button {
+  --btn-size: 65px;
+  border: none;
+  background: none;
+  position: absolute;
+  background-color: red;
+  height: var(--btn-size);
+  width: var(--btn-size);
+  top: 50%;
+  transform: translate(-50%);
+  z-index: 100;
 }
-.right-btn{
-    right: 29.3%;
+.right-btn {
+  right: 29.3%;
 }
 .cardList {
-    height: auto;
-    width: calc(3 * var(--card-width));
+  height: auto;
+  width: calc(3 * var(--card-width));
 }
-.card-wrapper{
-    position: relative;
-    width: 100%;
-    height: 100%;
-    perspective: 1000px;
+.card-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  perspective: 1000px;
 }
 .current--card {
-    width: 100%;
-    height: 100%;
-    display: inline-block;
-    user-select: none;
-    z-index: 50;
-    position: relative;
+  width: 100%;
+  height: 100%;
+  display: inline-block;
+  user-select: none;
+  z-index: 100;
+  position: relative;
 }
 .prev--card {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%) translateX(-220px) rotateY(25deg) scale(.9);
-    width: 100;
-    height: 100;
-    display: inline-block;
-    user-select: none;
-    opacity: 0.6;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%) translateX(-220px) rotateY(25deg) scale(0.9);
+  width: 100;
+  height: 100;
+  display: inline-block;
+  user-select: none;
+  opacity: 0.6;
 }
 .next--card {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%) translateX(220px) rotateY(-25deg) scale(.9);
-    width: 100%;
-    height: 100%;
-    display: inline-block;
-    user-select: none;
-    opacity: 0.6;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%) translateX(220px) rotateY(-25deg) scale(0.9);
+  width: 100%;
+  height: 100%;
+  display: inline-block;
+  user-select: none;
+  opacity: 0.6;
 }
-.card::before{
-    content: "";
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-    transition: opacity var(--card-transition-duration) var(--card-transition-easing);
+.card::before {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
 }
-
 </style>
--- msksi/pages/Gallery.vue
+++ msksi/pages/Gallery.vue
@@ -11,7 +11,7 @@ import GalleryTop from '~/components/GalleryTop.vue'
 import GalleryCarousel from '~/components/GalleryCarousel.vue'
 import NewCarousel from '~/components/NewCarousel.vue'
 export default {
-  components: { GalleryTop, GalleryCarousel, newcarousel },
+  components: { GalleryTop, GalleryCarousel, NewCarousel },
   head() {
     return {
       title: "Gallery | My Sister's Keeper Staten Island",
