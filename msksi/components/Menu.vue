9fa45d28919d634b51379482aedf3e31b421fa7e
IvyZ23 <71409315+IvyZ23@users.noreply.github.com>
2022-07-21 09:46:36 -0400

    Update Menu.vue

--- msksi/components/Menu.vue
+++ msksi/components/Menu.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="menu">
-    <input v-model="checked" @click="toggleOn" type="checkbox" id="active" />
+    <input v-model="checked" @click="toggle" type="checkbox" id="active" />
     <label for="active" class="menu-btn"><span></span></label>
     <label for="active" class="close"></label>
-    <div class="wrapper">
+    <div v-if="show" class="wrapper">
       <ul class="link-container">
-        <li class="link" @click="toggleOff">
+        <li class="link" @click="toggle">
           <router-link class="menu-router" to="/"> Home </router-link>
         </li>
-        <li class="link" @click="toggleOff">
+        <li class="link" @click="toggle">
           <router-link class="menu-router" to="/AboutUs">
             About Us
           </router-link>
         </li>
-        <li class="link" @click="toggleOff">
+        <li class="link" @click="toggle">
           <router-link class="menu-router" to="/Calendar">
             Calendar
           </router-link>
         </li>
-        <li class="link" @click="toggleOff">
+        <li class="link" @click="toggle">
           <router-link class="menu-router" to="/Gallery"> Gallery </router-link>
         </li>
-        <li class="link" @click="toggleOff">
+        <li class="link" @click="toggle">
           <router-link class="menu-router" to="/Resources">
             Resources
           </router-link>
         </li>
-        <li class="link" @click="toggleOff">
+        <li class="link" @click="toggle">
           <router-link class="menu-router" to="/JoinUs"> Join Us </router-link>
         </li>
       </ul>
@@ -38,22 +38,27 @@ export default {
   name: 'Menu',
   data(){
     return{
-      checked: Boolean,
+      checked: false,
+      show: false,
     }
   },
   methods: {
-    toggleOn() {
-      let menuOpen = document.querySelector('.wrapper');
-      menuOpen.classList.remove('hide');
-      console.log('bye')
-      this.checked = true;
-    },
-      toggleOff() {
-      let menuOpen = document.querySelector('.wrapper');
-      menuOpen.classList.add('hide')
-      console.log('hello')
-      this.checked = false;
+    toggle() {
+      this.show = !this.show
+      this.checked = !this.checked
     }
+    // toggleOn() {
+    //   let menuOpen = document.querySelector('.wrapper');
+    //   menuOpen.classList.remove('hide');
+      
+    //   this.checked = true;
+    // },
+    //   toggleOff() {
+    //   let menuOpen = document.querySelector('.wrapper');
+    //   menuOpen.classList.add('hide')
+  
+    //   this.checked = false;
+    // }
   },
 }
 </script>
