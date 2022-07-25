8b57a0d2d2b00791f632fe25e18652906664ae50
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2022-07-25 10:14:42 -0400

    Update nuxt.config.js

--- msksi/nuxt.config.js
+++ msksi/nuxt.config.js
@@ -36,15 +36,6 @@ export default {
     },
   },
   
-  generate: {
-    async routes () {
-      const { $content } = require('@nuxt/content')
-      const files = await $content().only(['path']).fetch()
-
-      return files.map(file => file.path === '/index' ? '/' : file.path)
-    }
-  },
-
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/content
