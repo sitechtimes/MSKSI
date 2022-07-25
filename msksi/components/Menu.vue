a3c931a9a5062461ff9397ac7cd6d67e67cd6b56
vvwenv <90654380+vvwenv@users.noreply.github.com>
2022-07-25 08:39:01 -0400

    menu animation

--- msksi/components/Menu.vue
+++ msksi/components/Menu.vue
@@ -3,41 +3,47 @@
     <input v-model="checked" @click="toggle" type="checkbox" id="active" />
     <label for="active" class="menu-btn"><span></span></label>
     <label for="active" class="close"></label>
-    <div v-if="show" class="wrapper">
-      <ul class="link-container">
-        <li class="link" @click="toggle">
-          <router-link class="menu-router" to="/"> Home </router-link>
-        </li>
-        <li class="link" @click="toggle">
-          <router-link class="menu-router" to="/AboutUs">
-            About Us
-          </router-link>
-        </li>
-        <li class="link" @click="toggle">
-          <router-link class="menu-router" to="/Calendar">
-            Calendar
-          </router-link>
-        </li>
-        <li class="link" @click="toggle">
-          <router-link class="menu-router" to="/Gallery"> Gallery </router-link>
-        </li>
-        <li class="link" @click="toggle">
-          <router-link class="menu-router" to="/Resources">
-            Resources
-          </router-link>
-        </li>
-        <li class="link" @click="toggle">
-          <router-link class="menu-router" to="/JoinUs"> Join Us </router-link>
-        </li>
-      </ul>
-    </div>
+    <Transition name="slide">
+      <div v-if="show" class="wrapper">
+        <ul class="link-container">
+          <li class="link" @click="toggle">
+            <router-link class="menu-router" to="/"> Home </router-link>
+          </li>
+          <li class="link" @click="toggle">
+            <router-link class="menu-router" to="/AboutUs">
+              About Us
+            </router-link>
+          </li>
+          <li class="link" @click="toggle">
+            <router-link class="menu-router" to="/Calendar">
+              Calendar
+            </router-link>
+          </li>
+          <li class="link" @click="toggle">
+            <router-link class="menu-router" to="/Gallery">
+              Gallery
+            </router-link>
+          </li>
+          <li class="link" @click="toggle">
+            <router-link class="menu-router" to="/Resources">
+              Resources
+            </router-link>
+          </li>
+          <li class="link" @click="toggle">
+            <router-link class="menu-router" to="/JoinUs">
+              Join Us
+            </router-link>
+          </li>
+        </ul>
+      </div></Transition
+    >
   </div>
 </template>
 <script>
 export default {
   name: 'Menu',
-  data(){
-    return{
+  data() {
+    return {
       checked: false,
       show: false,
     }
@@ -46,26 +52,43 @@ export default {
     toggle() {
       this.show = !this.show
       this.checked = !this.checked
-    }
+    },
     // toggleOn() {
     //   let menuOpen = document.querySelector('.wrapper');
     //   menuOpen.classList.remove('hide');
-      
+
     //   this.checked = true;
     // },
     //   toggleOff() {
     //   let menuOpen = document.querySelector('.wrapper');
     //   menuOpen.classList.add('hide')
-  
+
     //   this.checked = false;
     // }
   },
 }
 </script>
 <style>
-.hide {
+.slide-enter-active {
+  animation: slide-right 0.5s;
+}
+/* .slide-leave-active {
+  animation: slide-right 0.5s reverse;
+} */
+@keyframes slide-right {
+  0% {
+    right: -100%;
+  }
+  50% {
+    right: -100%;
+  }
+  100% {
+    right: 0;
+  }
+}
+/* .hide {
   z-index: -2;
   right: -100%;
   transition: all 0.6s ease-in-out;
-}
+} */
 </style>
