186b3759dace7ecb89e72725e4e11f9c89a6fe7c
IvyZ23 <71409315+IvyZ23@users.noreply.github.com>
2022-07-26 10:44:21 -0400

    Revert "fonts"

--- msksi/pages/Gallery.vue
+++ msksi/pages/Gallery.vue
@@ -15,11 +15,3 @@ export default {
   },
 }
 </script>
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=ABeeZee&family=Black+Han+Sans&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@600&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@500&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Dongle:wght@300&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Sora:wght@200&display=swap');
-</style>
